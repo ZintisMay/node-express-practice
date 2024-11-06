923c8288e6a3c6ede2ac06cb6e796b68b0f7862a
Zintis May <zintismay@gmail.com>
2024-11-06 14:04:47 -0500

    message service

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const fs = require("fs");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.json());
+
 const FILE_NAME = "array.txt";
-const array = loadFile() || [];
+const MESSAGES_FILE_NAME = "messages.txt";
+const array = loadFile(FILE_NAME) || [];
+const messages = loadFile(MESSAGES_FILE_NAME) || [];
+
+function addMessage(user, text) {
+  let m = {
+    user,
+    text,
+  };
+  messages.push(m);
+  saveFile(MESSAGES_FILE_NAME, messages);
+}
 
 app.get("/", function (req, res) {
   res.send("THIS IS MY SERVER");
@@ -14,12 +28,22 @@ app.get("/index", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/messages", function (req, res) {
+  res.json(messages);
+});
+
+app.post("/messages", function (req, res) {
+  console.log("req.body", req.body);
+  addMessage(req.body.user, req.body.text);
+  res.json(messages);
+});
+
 app.get("/info/:word", function (req, res) {
   let word = req.params.word;
   console.log(word);
   array.push(word);
   res.json(array);
-  saveFile(array);
+  saveFile(FILE_NAME, array);
 });
 
 app.get("/info", function (req, res) {
@@ -30,11 +54,11 @@ app.listen(3333, function () {
   console.log("app is listening on 3333");
 });
 
-function saveFile(data) {
-  fs.writeFileSync(FILE_NAME, JSON.stringify(data));
+function saveFile(fileName, data) {
+  fs.writeFileSync(fileName, JSON.stringify(data));
 }
 
-function loadFile(name) {
-  let data = fs.readFileSync(FILE_NAME);
+function loadFile(fileName) {
+  let data = fs.readFileSync(fileName);
   return JSON.parse(data.toString());
 }
